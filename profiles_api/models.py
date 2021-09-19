fee01885a244804e11ab4168aeed8a062409f7a1
Jesal <jesalchana23@gmail.com>
2021-09-19 12:42:40 +0200

    Enable django admin for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -16,27 +16,24 @@ class UserProfileManager(BaseUserManager):
         user = self.model(email=email, name=name,)
 
         user.set_password(password)
-        user.save(using = self._db)
+        user.save(using=self._db)
 
         return user
 
-def create_superuser(self,email, name, password):
-    """Create and save a new superuser with given details"""
-    user = self.create_user(email, name, password)
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
 
-    user.is_superuser = True
-    user.is_staff = True
-    user.save(using=self.self.db)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
 
-    return user
-
-
+        return user
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
-
-    email = models.EmailField(max_length=255 , unique=True)
+    email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -47,7 +44,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """Retrieve full name of user"""
+        """Retrieve full name for user"""
         return self.name
 
     def get_short_name(self):
@@ -55,5 +52,5 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         return self.name
 
     def __str__(self):
-        """return string representation of our user"""
+        """Return string representation of user"""
         return self.email
